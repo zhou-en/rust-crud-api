efe783908ecdabf01623a2c20e4e14706c4ac961
En Zhou <zhouen.nathan@gmail.com>
2023-05-20 10:44:16 -0600

    add more handle functions

--- src/main.rs
+++ src/main.rs
@@ -107,6 +107,55 @@ fn handle_get_request(request: &str) -> (String, String) {
     }
 }
 
+fn handle_get_all_request(_request: &str) -> (String, String) {
+    match Client::connect(DB_URL, NoTls) {
+        Ok(mut client) => {
+            let mut users = Vec::new();
+            for row in client.query("SELECT id, name, email FROM users", &[]).unwrap() {
+                users.push(User {
+                    id: row.get(0),
+                    name: row.get(1),
+                    email: row.get(2),
+                });
+            }
+            (OK_RESPONSE.to_string(), serde_json::to_string(&users).unwrap())
+        }
+        _ => (INTERNAL_SERVER_ERROR.to_string(), "Internal Server Error".to_string()),
+    }
+}
+
+fn handle_put_request(request: &str) -> (String, String) {
+    match (
+        get_id(&request).parse::<i32>(),
+        get_user_request_body(&request),
+        Client::connect(DB_URL, NoTls)
+    ) {
+        (Ok(id), Ok(user), Ok(mut client)) => {
+            client.execute(
+                "UPDATE users SET name = $1, email = $2 WHERE id = $3",
+                &[&user.name, &user.email, &id],
+            ).unwrap();
+            (OK_RESPONSE.to_string(), "User updated".to_string())
+        }
+        _ => (INTERNAL_SERVER_ERROR.to_string(), "Internal Server Error".to_string()),
+    }
+}
+
+fn handle_delete_request(request: &str) -> (String, String) {
+    match (get_id(&request).parse::<i32>(), Client::connect(DB_URL, NoTls)) {
+        (Ok(id), Ok(mut client)) => {
+            let rows_affected = client.execute("DELETE FROM users WHERE id = $1", &[&id]).unwrap();
+
+            if rows_affected == 0 {
+                return (NOT_FOUND.to_string(), "User not found".to_string());
+            }
+
+            (OK_RESPONSE.to_string(), "User deleted".to_string())
+        }
+        _ => (INTERNAL_SERVER_ERROR.to_string(), "Internal Server Error".to_string()),
+    }
+}
+
 
 fn set_database() -> Result<(), PostgresError> {
     let mut client = Client::connect(DB_URL, NoTls)?;
